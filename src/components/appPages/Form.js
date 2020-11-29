39d75e34babe02a98efc642b28563b249e2c8009
Robin Zimmer <robinzimmer1989@web.de>
2020-11-28 16:51:47 -0800

    remove legacy form components

--- src/components/appPages/Form.js
+++ src/components/appPages/Form.js
@@ -4,9 +4,6 @@ import { Button, Empty } from 'antd'
 // import app components
 import CmsLayout from '../CmsLayout'
 import PageWrapper from '../PageWrapper'
-import FormEditorSidebar from '../formEditor/FormEditorSidebar'
-import FormFieldsForm from '../formEditor/FormFieldsForm'
-import FormFlexibleContent from '../formEditor/FormFlexibleContent'
 
 import { convertToPropsSchema } from '../../utils'
 import { useStore } from '../../store'
@@ -35,40 +32,7 @@ const Form = (props) => {
     }
   }, [formID])
 
-  const handleOpenDialog = () =>
-    dispatch({
-      type: 'SET_DIALOG',
-      payload: {
-        open: true,
-        title: 'Choose a Block',
-        component: <FormFieldsForm index={0} />,
-        width: 800,
-      },
-    })
-
-  return (
-    <CmsLayout pageTitle="Form" actionBar="editor" rightSidebar={<FormEditorSidebar />}>
-      {/* <PageWrapper theme={theme}>
-        {form?.content?.length > 0 ? (
-          <FormFlexibleContent blocks={convertToPropsSchema(form.content)} />
-        ) : (
-          <Empty
-            style={{ padding: 60 }}
-            image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
-            imageStyle={{
-              height: 60,
-            }}
-            description=""
-            className="flexible-content-empty"
-          >
-            <Button type="primary" onClick={handleOpenDialog}>
-              Add Field
-            </Button>
-          </Empty>
-        )}
-      </PageWrapper> */}
-    </CmsLayout>
-  )
+  return <CmsLayout pageTitle="Form" actionBar="editor"></CmsLayout>
 }
 
 export default Form
