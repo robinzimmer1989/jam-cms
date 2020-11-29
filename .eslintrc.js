96de09e1dc87d5ebd5e634c0e76cc69216768c2e
Robin Zimmer <robinzimmer1989@web.de>
2020-11-29 13:17:49 -0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  requireConfigFile: false,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'react/prop-types': 1,
+  },
+};
