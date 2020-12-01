f63ad5f7d1858bb4d066a747f398d5bcf3cfb149
Robin Zimmer <robinzimmer1989@web.de>
2020-11-30 16:12:46 -0800

    move deployment badge logic to separate file and enable auto update

--- jam-cms/src/components/CmsHeader.js
+++ jam-cms/src/components/CmsHeader.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-import { PageHeader, Button, notification, Tag } from 'antd';
-import axios from 'axios';
+import { PageHeader, Button, Tag } from 'antd';
 
 // import app components
+import DeploymentBadge from './DeploymentBadge';
 import ViewToggle from './ViewToggle';
 import AvatarMenu from './AvatarMenu';
 import { useStore } from '../store';
@@ -20,18 +19,6 @@ const CmsHeader = (props) => {
 
   const site = sites[siteID] || null;
 
-  const handleDeploy = async () => {
-    if (!site?.frontPage) {
-      return notification.error({
-        message: 'Error',
-        description: 'Please add a front page',
-        placement: 'bottomRight',
-      });
-    }
-
-    axios.post(site.deploymentBuildHook);
-  };
-
   const tags = [];
 
   if (actionBar === 'editor' && hasChanged) {
@@ -43,22 +30,23 @@ const CmsHeader = (props) => {
   if (actionBar === 'editor') {
     buttons.push(<ViewToggle key={'view-toggle'} />);
   } else {
-    if (site?.deploymentBadgeImage) {
-      buttons.push(
-        <DeploymentStatus
-          key="deployment-status"
-          src={`${site.deploymentBadgeImage}?key=${Math.floor(Math.random() * Math.floor(100))}`}
-        />
-      );
-    }
-
-    if (site?.deploymentBuildHook) {
-      buttons.push(<Button key="deployment-button" size="small" children={`Deploy`} onClick={handleDeploy} />);
-    }
+    buttons.push(
+      <DeploymentBadge
+        key="deployment-badge"
+        deploymentBadgeImage={site.deploymentBadgeImage}
+        deploymentBuildHook={site.deploymentBuildHook}
+      />
+    );
 
     if (site?.frontendUrl) {
       buttons.push(
-        <Button key="visit-site-button" size="small" children={`Visit Site`} href={site?.frontendUrl} target="_blank" />
+        <Button
+          key="visit-site-button"
+          size="small"
+          children={`Visit Site`}
+          href={site?.frontendUrl}
+          target="_blank"
+        />
       );
     }
 
@@ -67,10 +55,5 @@ const CmsHeader = (props) => {
 
   return <PageHeader title={title} extra={buttons} tags={tags} />;
 };
-
-const DeploymentStatus = styled.img`
-  height: 16px;
-  margin: 0 10px;
-`;
 
 export default CmsHeader;
---
+++ jam-cms/src/components/DeploymentBadge.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Space, Button, notification } from 'antd';
+import axios from 'axios';
+
+// import app components
+import { useStore } from '../store';
+
+const DeploymentBadge = (props) => {
+  const { deploymentBadgeImage, deploymentBuildHook } = props;
+
+  const [
+    {
+      cmsState: { siteID, sites },
+    },
+  ] = useStore();
+
+  const site = sites[siteID];
+
+  const [src, setSrc] = useState(deploymentBadgeImage);
+
+  useEffect(() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      setSrc(`${deploymentBadgeImage}?v=${Math.floor(Math.random() * Math.floor(100))}`);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleDeploy = async () => {
+    if (!site?.frontPage) {
+      return notification.error({
+        message: 'Error',
+        description: 'Please add a front page',
+        placement: 'bottomRight',
+      });
+    }
+
+    axios.post(site.deploymentBuildHook);
+
+    setSrc(`${deploymentBadgeImage}?v=${Math.floor(Math.random() * Math.floor(100))}`);
+  };
+
+  return (
+    <Space>
+      {deploymentBadgeImage && <DeploymentStatus src={src} />}
+      {deploymentBuildHook && <Button size="small" children={`Deploy`} onClick={handleDeploy} />}
+    </Space>
+  );
+};
+
+const DeploymentStatus = styled.img`
+  height: 16px;
+  margin: 0 10px;
+`;
+
+export default DeploymentBadge;
