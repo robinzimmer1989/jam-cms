0c7fd9c0cc68a1343966cf951c3c9ec55d7ea891
Robin Zimmer <robinzimmer1989@web.de>
2020-12-01 20:14:15 -0800

    update edges props

--- jam-cms/src/components/Edges.js
+++ jam-cms/src/components/Edges.js
@@ -4,15 +4,15 @@ import styled from 'styled-components';
 const Edges = (props) => <Container {...props}>{props.children}</Container>;
 
 const Container = styled.div`
-  width: ${(props) => (props.size === `none` ? `100%` : `90%`)};
+  width: 100%;
+  padding: 0 2.5%;
   margin: 0 auto;
 
   ${(props) => props.size === `xs` && `max-width: 460px;`}
   ${(props) => props.size === `sm` && `max-width: 600px;`}
-    ${(props) => props.size === `md` && `max-width: 960px;`}
-    ${(props) => props.size === `lg` && `max-width: 1280px;`}
-    ${(props) => props.size === `xl` && `max-width: 1440px;`}
-    ${(props) => props.size === `none` && `max-width: 100%;`}
+  ${(props) => props.size === `md` && `max-width: 1024px;`}
+  ${(props) => props.size === `lg` && `max-width: 1280px;`}
+  ${(props) => props.size === `xl` && `max-width: 1440px;`}
 `;
 
 export default Edges;
