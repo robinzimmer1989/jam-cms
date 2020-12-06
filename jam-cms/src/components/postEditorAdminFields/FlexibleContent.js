97b7622e6edfdcd07a04dabbb7227cb9d0b9f69b
Robin Zimmer <robinzimmer1989@web.de>
2020-12-06 10:37:56 -0800

    add move functionality to flexible content field

--- jam-cms/src/components/postEditorAdminFields/FlexibleContent.js
+++ jam-cms/src/components/postEditorAdminFields/FlexibleContent.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Collapse, Button, Space, Menu, Dropdown } from 'antd';
 import produce from 'immer';
+import { UpCircleTwoTone, DownCircleTwoTone } from '@ant-design/icons';
 
 // import app components
 import { getField } from '../BlockEditFields';
@@ -45,6 +47,20 @@ const FlexibleContent = (props) => {
     onChange(newValues);
   };
 
+  const handleMoveElement = (index, newIndex) => {
+    const newValues = produce(values, (draft) => {
+      if (newIndex > -1 && newIndex < draft.length) {
+        const temp = draft[index];
+        draft[index] = draft[newIndex];
+        draft[newIndex] = temp;
+      }
+
+      return draft;
+    });
+
+    onChange(newValues);
+  };
+
   const menuItems = items.map((o) => (
     <Menu.Item key={o.id} children={o.label} onClick={() => handleAdd(o.id, items.length)} />
   ));
@@ -52,49 +68,95 @@ const FlexibleContent = (props) => {
   const menu = <Menu>{menuItems}</Menu>;
 
   return (
-    <Space direction="vertical">
-      <Space direction="vertical" size={10}>
-        {values &&
-          values.map((value, index) => {
-            const layout = items.find((o) => o.id === value.id);
+    <Container>
+      <Space direction="vertical">
+        <Space direction="vertical" size={10}>
+          {values &&
+            values.map((value, index) => {
+              const layout = items.find((o) => o.id === value.id);
 
-            return (
-              <Collapse key={index}>
-                <Collapse.Panel header={layout?.label || 'NA'}>
-                  <Space direction="vertical" size={30}>
-                    {layout?.fields &&
-                      layout.fields.map((field, fieldIndex) => {
-                        return (
-                          <div key={field.id}>
-                            {getField({
-                              field: { ...field, value: value.fields[fieldIndex].value },
-                              index,
-                              site,
-                              onChangeElement: (value) => handleChange(value, index, fieldIndex),
-                              dispatch,
-                            })}
-                          </div>
-                        );
-                      })}
+              return (
+                <Collapse key={index}>
+                  <Collapse.Panel
+                    header={layout?.label || 'NA'}
+                    extra={
+                      <MoveIcons className={`icon`} onClick={(e) => e.stopPropagation()}>
+                        <MoveIcon
+                          onClick={() => handleMoveElement(index, index - 1)}
+                          disabled={index === 0}
+                        >
+                          <UpCircleTwoTone />
+                        </MoveIcon>
 
-                    <Button
-                      size="small"
-                      danger
-                      children={`Remove`}
-                      onClick={() => handleRemove(index)}
-                    />
-                  </Space>
-                </Collapse.Panel>
-              </Collapse>
-            );
-          })}
+                        <MoveIcon
+                          onClick={() => handleMoveElement(index, index + 1)}
+                          disabled={index === values.length - 1}
+                        >
+                          <DownCircleTwoTone />
+                        </MoveIcon>
+                      </MoveIcons>
+                    }
+                  >
+                    <Space direction="vertical" size={30}>
+                      {layout?.fields &&
+                        layout.fields.map((field, fieldIndex) => {
+                          return (
+                            <div key={field.id}>
+                              {getField({
+                                field: { ...field, value: value.fields[fieldIndex].value },
+                                index,
+                                site,
+                                onChangeElement: (value) => handleChange(value, index, fieldIndex),
+                                dispatch,
+                              })}
+                            </div>
+                          );
+                        })}
+
+                      <Button
+                        size="small"
+                        danger
+                        children={`Remove`}
+                        onClick={() => handleRemove(index)}
+                      />
+                    </Space>
+                  </Collapse.Panel>
+                </Collapse>
+              );
+            })}
+        </Space>
+
+        <Dropdown overlay={menu}>
+          <Button>Add</Button>
+        </Dropdown>
       </Space>
-
-      <Dropdown overlay={menu}>
-        <Button>Add</Button>
-      </Dropdown>
-    </Space>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  .ant-collapse-item {
+    position: relative;
+  }
+`;
+
+const MoveIcons = styled.div`
+  position: absolute;
+  z-index: 2;
+  right: 5px;
+  top: 0;
+  height: 100%;
+  display: flex;
+`;
+
+const MoveIcon = styled.div`
+  width: 20px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  opacity: ${(props) => (props.disabled ? 0.4 : 1)};
+`;
 
 export default FlexibleContent;
