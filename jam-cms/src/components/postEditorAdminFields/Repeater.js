5ba5f2e5b7a90835149f112c830c5fff9c44f658
Robin Zimmer <robinzimmer1989@web.de>
2020-12-08 22:01:35 -0800

    add move icons to repeater field

--- jam-cms/src/components/postEditorAdminFields/Repeater.js
+++ jam-cms/src/components/postEditorAdminFields/Repeater.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Collapse, Button, Space } from 'antd';
 import produce from 'immer';
+import { UpCircleTwoTone, DownCircleTwoTone } from '@ant-design/icons';
 
 // import app components
 import { getField } from '../BlockEditFields';
@@ -37,46 +39,106 @@ const Repeater = (props) => {
     onChange(newValues);
   };
 
+  const handleMoveElement = (index, newIndex) => {
+    const newValues = produce(values, (draft) => {
+      if (newIndex > -1 && newIndex < draft.length) {
+        const temp = draft[index];
+        draft[index] = draft[newIndex];
+        draft[newIndex] = temp;
+      }
+
+      return draft;
+    });
+
+    onChange(newValues);
+  };
+
   return (
-    <Space direction="vertical">
-      <Space direction="vertical" size={10}>
-        {values &&
-          values.map((value, index) => {
-            return (
-              <Collapse key={index}>
-                <Collapse.Panel header={`Item ${index + 1}`}>
-                  <Space direction="vertical" size={30}>
-                    {items &&
-                      items.map((field, subIndex) => {
-                        return (
-                          <div key={subIndex}>
-                            {getField({
-                              field: { ...field, value: value[field.id] },
-                              index,
-                              site,
-                              onChangeElement: (value) => handleChange(value, index),
-                              dispatch,
-                            })}
-                          </div>
-                        );
-                      })}
+    <Container>
+      <Space direction="vertical">
+        <Space direction="vertical" size={10}>
+          {values &&
+            values.map((value, index) => {
+              return (
+                <Collapse key={index}>
+                  <Collapse.Panel
+                    header={`Item ${index + 1}`}
+                    extra={
+                      <MoveIcons className={`icon`} onClick={(e) => e.stopPropagation()}>
+                        <MoveIcon
+                          onClick={() => handleMoveElement(index, index - 1)}
+                          disabled={index === 0}
+                        >
+                          <UpCircleTwoTone />
+                        </MoveIcon>
 
-                    <Button
-                      size="small"
-                      danger
-                      children={`Remove`}
-                      onClick={() => handleRemove(index)}
-                    />
-                  </Space>
-                </Collapse.Panel>
-              </Collapse>
-            );
-          })}
+                        <MoveIcon
+                          onClick={() => handleMoveElement(index, index + 1)}
+                          disabled={index === values.length - 1}
+                        >
+                          <DownCircleTwoTone />
+                        </MoveIcon>
+                      </MoveIcons>
+                    }
+                  >
+                    <Space direction="vertical" size={30}>
+                      {items &&
+                        items.map((field, subIndex) => {
+                          return (
+                            <div key={subIndex}>
+                              {getField({
+                                field: { ...field, value: value[field.id] },
+                                index,
+                                site,
+                                onChangeElement: (value) => handleChange(value, index),
+                                dispatch,
+                              })}
+                            </div>
+                          );
+                        })}
+
+                      <Button
+                        size="small"
+                        danger
+                        children={`Remove`}
+                        onClick={() => handleRemove(index)}
+                      />
+                    </Space>
+                  </Collapse.Panel>
+                </Collapse>
+              );
+            })}
+        </Space>
+
+        <Button onClick={() => handleAdd(items.length)}>Add</Button>
       </Space>
-
-      <Button onClick={() => handleAdd(items.length)}>Add</Button>
-    </Space>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  .ant-collapse-item {
+    position: relative;
+  }
+`;
+
+const MoveIcons = styled.div`
+  position: absolute;
+  z-index: 2;
+  right: 5px;
+  top: 0;
+  height: 100%;
+  display: flex;
+`;
+
+const MoveIcon = styled.div`
+  width: 20px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  opacity: ${(props) => (props.disabled ? 0.4 : 1)};
+`;
 
 export default Repeater;
