b912ebc7aac21a58d6d01711ad8e196c7fe3ea96
Robin Zimmer <robinzimmer1989@web.de>
2020-12-08 21:05:19 -0800

    move save button to header and remove unnecessary skeleton loaders

--- jam-cms/src/components/PostSettings.js
+++ jam-cms/src/components/PostSettings.js
@@ -1,20 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import produce from 'immer';
-import { Button, Space, Select as AntSelect, Checkbox, notification } from 'antd';
+import { Space, Select as AntSelect, Checkbox } from 'antd';
 import { set } from 'lodash';
 
 // import app components
 import Input from './Input';
 import Select from './Select';
 import Caption from './Caption';
-import Skeleton from './Skeleton';
 import PostTreeSelect from './PostTreeSelect';
 import MediaLibrary from './MediaLibrary';
 import ImagePicker from './postEditorAdminFields/ImagePicker';
 
 import { useStore } from '../store';
-import { postActions, siteActions } from '../actions';
 import { generateSlug } from '../utils';
 
 const PostSettings = () => {
@@ -26,29 +24,12 @@ const PostSettings = () => {
     dispatch,
   ] = useStore();
 
-  const [loading, setLoading] = useState(false);
-
   const postType = sites[siteID]?.postTypes?.[post?.postTypeID];
   const posts = postType?.posts;
 
   // Remove own post for display in the page parent drop down
   const otherPosts = { ...posts };
   post && delete otherPosts[post.id];
-
-  const handleSavePost = async () => {
-    const { id, settings, frontPage } = site;
-
-    setLoading(true);
-    await postActions.updatePost({ siteID: id, ...post }, dispatch);
-    await siteActions.updateSite({ id, settings, frontPage }, dispatch);
-    setLoading(false);
-
-    notification.success({
-      message: 'Success',
-      description: 'Updated successfully',
-      placement: 'bottomRight',
-    });
-  };
 
   const handleChangePost = (name, value) => {
     const nextPost = produce(post, (draft) => set(draft, `${name}`, value));
@@ -77,111 +58,87 @@ const PostSettings = () => {
   return (
     <Container>
       <Space direction="vertical">
-        <Skeleton done={!!post} height={32}>
-          <Input
-            value={post?.title || ''}
-            onChange={(e) => handleChangePost('title', e.target.value)}
-            label={'Title'}
-          />
-        </Skeleton>
+        <Input
+          value={post?.title || ''}
+          onChange={(e) => handleChangePost('title', e.target.value)}
+          label={'Title'}
+        />
 
-        <Skeleton done={!!post} height={32}>
-          <Input
-            value={post?.id === site?.frontPage ? '/' : post?.slug || ''}
-            onChange={(e) => handleChangePost('slug', e.target.value)}
-            label={'Slug'}
-            disabled={post?.id === site?.frontPage}
-          />
-        </Skeleton>
+        <Input
+          value={post?.id === site?.frontPage ? '/' : post?.slug || ''}
+          onChange={(e) => handleChangePost('slug', e.target.value)}
+          label={'Slug'}
+          disabled={post?.id === site?.frontPage}
+        />
 
         {post?.id !== site?.frontPage && (
-          <Skeleton done={!!post} height={32}>
-            <Input
-              value={generateSlug(postType, post?.id, site?.frontPage)}
-              label={'Permalink'}
-              disabled
-            />
-          </Skeleton>
+          <Input
+            value={generateSlug(postType, post?.id, site?.frontPage)}
+            label={'Permalink'}
+            disabled
+          />
         )}
 
-        <Skeleton done={!!post} height={32}>
-          <Select
-            value={post?.status || ''}
-            onChange={(value) => handleChangePost('status', value)}
-            label={'Status'}
-          >
-            <AntSelect.Option value={'publish'} children={'Publish'} />
-            <AntSelect.Option value={'draft'} children={'Draft'} />
-            <AntSelect.Option value={'trash'} children={'Trash'} />
-          </Select>
-        </Skeleton>
+        <Select
+          value={post?.status || ''}
+          onChange={(value) => handleChangePost('status', value)}
+          label={'Status'}
+        >
+          <AntSelect.Option value={'publish'} children={'Publish'} />
+          <AntSelect.Option value={'draft'} children={'Draft'} />
+          <AntSelect.Option value={'trash'} children={'Trash'} />
+        </Select>
 
         {post?.postTypeID === 'page' && (
-          <Skeleton done={!!post} height={32}>
-            <PostTreeSelect
-              label="Parent"
-              items={Object.values(otherPosts)}
-              value={post?.parentID}
-              onChange={(value) => handleChangePost('parentID', value)}
-            />
-          </Skeleton>
+          <PostTreeSelect
+            label="Parent"
+            items={Object.values(otherPosts)}
+            value={post?.parentID}
+            onChange={(value) => handleChangePost('parentID', value)}
+          />
         )}
 
-        <Skeleton done={!!post} height={32}>
-          <Input
-            value={post?.seoTitle || ''}
-            onChange={(e) => handleChangePost('seoTitle', e.target.value)}
-            label={'SEO Title'}
-          />
-        </Skeleton>
+        <Input
+          value={post?.seoTitle || ''}
+          onChange={(e) => handleChangePost('seoTitle', e.target.value)}
+          label={'SEO Title'}
+        />
 
-        <Skeleton done={!!post} height={98}>
-          <Input
-            value={post?.seoDescription || ''}
-            onChange={(e) => handleChangePost('seoDescription', e.target.value)}
-            label={'SEO Description'}
-            rows={4}
-          />
-        </Skeleton>
+        <Input
+          value={post?.seoDescription || ''}
+          onChange={(e) => handleChangePost('seoDescription', e.target.value)}
+          label={'SEO Description'}
+          rows={4}
+        />
 
         {post?.postTypeID !== 'page' && (
-          <Skeleton done={!!post} height={100}>
-            <Space direction="vertical" size={2}>
-              <Caption children="Featured Image" />
-              <ImagePicker
-                value={post?.featuredImage}
-                onRemove={() => handleSelectImage(null)}
-                onClick={() =>
-                  dispatch({
-                    type: `SET_DIALOG`,
-                    payload: {
-                      open: true,
-                      component: <MediaLibrary onSelect={handleSelectImage} allow={['image']} />,
-                      width: 1000,
-                    },
-                  })
-                }
-              />
-            </Space>
-          </Skeleton>
+          <Space direction="vertical" size={2}>
+            <Caption children="Featured Image" />
+            <ImagePicker
+              value={post?.featuredImage}
+              onRemove={() => handleSelectImage(null)}
+              onClick={() =>
+                dispatch({
+                  type: `SET_DIALOG`,
+                  payload: {
+                    open: true,
+                    component: <MediaLibrary onSelect={handleSelectImage} allow={['image']} />,
+                    width: 1000,
+                  },
+                })
+              }
+            />
+          </Space>
         )}
 
         {post?.postTypeID === 'page' && (
-          <Skeleton done={!!post} height={98}>
-            <Checkbox
-              value={post?.id}
-              checked={post?.id === site?.frontPage}
-              onChange={(e) =>
-                handleChangeSite('frontPage', e.target.checked ? e.target.value : '')
-              }
-              children="Front Page"
-            />
-          </Skeleton>
+          <Checkbox
+            value={post?.id}
+            checked={post?.id === site?.frontPage}
+            onChange={(e) => handleChangeSite('frontPage', e.target.checked ? e.target.value : '')}
+            children="Front Page"
+          />
         )}
-
-        <Skeleton done={!!post} height={32} width={78}>
-          <Button children="Update" type="primary" onClick={handleSavePost} loading={loading} />
-        </Skeleton>
       </Space>
     </Container>
   );
