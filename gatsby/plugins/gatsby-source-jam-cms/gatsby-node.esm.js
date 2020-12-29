581171d04b6cee57c93dbe1d7346b2fed5c86f3f
Robin Zimmer <robinzimmer1989@web.de>
2020-12-29 12:13:25 -0500

    update source plugin to 0.0.3

--- gatsby/plugins/gatsby-source-jam-cms/gatsby-node.esm.js
+++ gatsby/plugins/gatsby-source-jam-cms/gatsby-node.esm.js
@@ -1,24 +1,25 @@
 const axios = require('axios');
 const path = require('path');
+const fs = require('fs');
 
 exports.createPages = async ({ actions, reporter }, pluginOptions) => {
   const { createPage } = actions;
-  const { template, source, apiKey } = pluginOptions;
+  const { source, apiKey } = pluginOptions;
 
   if (!source) {
-    reporter.error('Source url is required');
+    reporter.error('jamCMS: Source url is required');
     return;
   }
 
   if (!apiKey) {
-    reporter.error('Api key is required');
+    reporter.error('jamCMS: Api key is required');
     return;
   }
 
   let activity;
 
   try {
-    activity = reporter.activityTimer(`Fetching data...`);
+    activity = reporter.activityTimer('jamCMS: Fetching data...');
     activity.start();
 
     const url = `${source.replace(/\/+$/, '')}/getBuildSite?apiKey=${apiKey}`;
@@ -27,23 +28,32 @@ exports.createPages = async ({ actions, reporter }, pluginOptions) => {
     activity.end();
 
     const {
-      data: { header, footer, posts },
+      data: { settings, posts },
     } = await response;
 
-    const templatePath = path.resolve(template);
+    const missingTemplates = {};
 
     await Promise.all(
       posts &&
-        posts.map(async (post) => {
-          await createPage({
-            path: post.slug,
-            component: templatePath,
-            context: {
-              header,
-              footer,
-              post,
-            },
-          });
+        posts.map(async (o) => {
+          const templatePath = path.resolve(`./src/templates/${o.postTypeID}/${o.template}.js`);
+
+          if (fs.existsSync(templatePath)) {
+            await createPage({
+              path: o.slug,
+              component: templatePath,
+              context: {
+                globalOptions: settings,
+                content: o.content,
+              },
+            });
+          } else {
+            // Only show error message about missing template once
+            if (!missingTemplates[`${o.postTypeID}/${o.template}`]) {
+              reporter.error(`Template ${o.postTypeID}/${o.template}.js not found`);
+              missingTemplates[`${o.postTypeID}/${o.template}`] = true;
+            }
+          }
         })
     );
   } catch (err) {
