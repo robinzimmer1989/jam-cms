44952498c8a7353a512378c75c3af03a896105fc
Robin Zimmer <robinzimmer1989@web.de>
2020-12-30 12:54:57 -0500

    make links unclickable in iframe

--- jam-cms/src/components/Iframe.js
+++ jam-cms/src/components/Iframe.js
@@ -44,45 +44,8 @@ const Container = styled.div`
 const ThemeStyles = createGlobalStyle`
   ${({ theme }) => theme.css}
 
-  .blockName {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji' !important;
-  }
-
-  .flexible-content-empty {
-    font-size: 14px !important;
-
-    .ant-empty-image, .ant-empty-footer {
-      display: flex;
-      justify-content: center;
-    }
-
-    .ant-empty-image {
-      margin-bottom: 20px;
-    }
-
-    .ant-btn-primary {
-      background: #1890ff;
-      border: 1px solid #1890ff;
-      text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
-      box-shadow: 0 2px 0 rgba(0, 0, 0, 0.045);
-      height: 32px;
-      padding: 4px 15px;
-      border-radius: 2px;
-      outline: 0;
-      cursor: pointer;
-
-      &:hover {
-        color: #fff;
-        background: #40a9ff;
-        border-color: #40a9ff;
-      }
-
-      span {
-        color: #fff;
-        font-size: 14px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji' !important;
-      }
-    }
+  a {
+    pointer-events: none;
   }
 `;
 
