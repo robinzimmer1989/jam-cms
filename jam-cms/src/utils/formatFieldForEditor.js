5e14e5b3ec7c9c13067026b2c22c7040792305da
Robin Zimmer <robinzimmer1989@web.de>
2020-12-31 09:05:59 -0500

    add group and repeater support to formatFieldForEditor function

--- jam-cms/src/utils/formatFieldForEditor.js
+++ jam-cms/src/utils/formatFieldForEditor.js
@@ -3,6 +3,10 @@ import produce from 'immer';
 
 import generateSlug from './generateSlug';
 
+// This function formats the value for the editor.
+// Especially for collections this ie necessary, because the value is stored as a string
+// and the post elements are added on the fly
+
 export default function formatFieldForEditor(field, site) {
   // Post relationship fields
   if (field.type === 'collection' && field?.value) {
@@ -33,6 +37,42 @@ export default function formatFieldForEditor(field, site) {
       ...field,
       value: 'Write something...',
     };
+  }
+
+  if (field.type === 'group') {
+    const nextGroupField = produce(field, (draft) => {
+      Object.keys(draft.value).map((key) => {
+        const subField = draft.fields.find((p) => p.id === key);
+
+        const formattedSubField = formatFieldForEditor(
+          { ...subField, value: draft.value[key] },
+          site
+        );
+
+        return set(draft, `value.${key}`, formattedSubField.value);
+      });
+    });
+
+    return nextGroupField;
+  }
+
+  if (field.type === 'repeater') {
+    const nextRepeaterField = produce(field, (draft) => {
+      draft.value.map((o, i) => {
+        Object.keys(o).map((key) => {
+          const subField = draft.items.find((p) => p.id === key);
+
+          const formattedSubField = formatFieldForEditor(
+            { ...subField, value: draft.value[i][key] },
+            site
+          );
+
+          return set(draft, `value.${i}.${key}`, formattedSubField.value);
+        });
+      });
+    });
+
+    return nextRepeaterField;
   }
 
   if (field.type === 'flexible_content') {
