00f1f45e66900d1fac45afa1739df7e592eb4429
Robin Zimmer <robinzimmer1989@web.de>
2021-01-07 08:18:25 -0800

    move minireset into jamCMS and tweak global styles

--- jam-cms/src/theme/CmsStyles.js
+++ jam-cms/src/theme/CmsStyles.js
@@ -1,118 +1,109 @@
 import { createGlobalStyle } from 'styled-components';
 
 import colors from './colors';
+import minireset from './styles/minireset';
 
 export default createGlobalStyle`
-
-  body, iframe {
-    background: ${colors.background.light};
-    overflow-y: auto !important;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  * {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    box-sizing: border-box;
-  }
-
-  img {
-    margin: 0;
-  }
-
-  .ant-layout {
-    && {
-      background: transparent;
-    }
-  }
-
-  .ant-layout-header {
-    height: 50px;
-    background: #fff;
-    padding: 0;
-  }
-
-  .ant-page-header-content {
-    padding-bottom: 12px;
-  }
-
-  .ant-layout-content {
-    display: flex;
-  }
-
-  .ant-layout-footer {
-    background: transparent;
-  }
-
-  .ant-layout-sider {
-    box-shadow: 0 8px 15px rgba(29, 46, 83, 0.07);
-  }
-
-  .ant-page-header {
-    padding: 5px 0;
-  }
-
-  .ant-card {
-    box-shadow: 0 8px 15px rgba(29, 46, 83, 0.07);
-  }
-
-  .ant-space-vertical {
-    width: 100%;
-  }
-
-  .ant-select {
-    width: 100%;
-  }
-
-  .ant-page-header-heading-extra {
-    display: flex;
-    align-items: center;
-  }
-
-  .ant-tabs-tab-btn {
-    font-size: 11px;
-  }
-
-  .ant-menu-item, .ant-menu-submenu-title {
-    padding-left: 32px !important;
-  }
-
-  .ant-menu-item .anticon, .ant-menu-submenu-title .anticon {
-    svg {
-      width: 14px;
-      height: 14px;
-    }
-  }
+  ${minireset}
 
   .ant-modal-body {
     background: ${colors.background.light};
   }
+  
+  #jam-cms {
+    background: ${colors.background.light};
 
-  .ant-collapse {
-    border: none;
-    background: #f0f0f0;
-  }
+    a {
+      text-decoration: none;
+    }
 
-  .ant-collapse > .ant-collapse-item {
-    border: none;
-  }
+    img {
+      margin: 0;
+    }
 
-  .ant-collapse > .ant-collapse-item > .ant-collapse-header {
-    padding: 6px 16px 6px 30px;
-  }
+    .ant-layout-header {
+      height: 50px;
+      background: #fff;
+      padding: 0;
+    }
 
-  .ant-collapse > .ant-collapse-item > .ant-collapse-header .ant-collapse-arrow {
-    top: 50%;
-    transform: translateY(-50%);
-    left: 10px;
-  }
+    .ant-page-header {
+      padding: 5px 0;
+    }
 
-  .ant-collapse-content > .ant-collapse-content-box {
-    padding: 0;
-    background: #f7f7f7;
-    border-bottom: 1px solid #d9d9d9;
+    .ant-page-header-content {
+      padding-bottom: 12px;
+    }
+
+    .ant-layout {
+      background: transparent;
+    }
+
+    .ant-layout-content {
+      display: flex;
+    }
+
+    .ant-layout-footer {
+      background: transparent;
+    }
+
+    .ant-layout-sider {
+      box-shadow: 0 8px 15px rgba(29, 46, 83, 0.07);
+    }
+
+    .ant-card {
+      box-shadow: 0 8px 15px rgba(29, 46, 83, 0.07);
+    }
+
+    .ant-space-vertical {
+      width: 100%;
+    }
+
+    .ant-select {
+      width: 100%;
+    }
+
+    .ant-page-header-heading-extra {
+      display: flex;
+      align-items: center;
+    }
+
+    .ant-tabs-tab-btn {
+      font-size: 11px;
+    }
+
+    .ant-menu-item, .ant-menu-submenu-title {
+      padding-left: 32px !important;
+    }
+
+    .ant-menu-item .anticon, .ant-menu-submenu-title .anticon {
+      svg {
+        width: 14px;
+        height: 14px;
+      }
+    }
+
+    .ant-collapse {
+      position: relative;
+      background-color: ${colors.background.light};
+    }
+
+    .ant-collapse > .ant-collapse-item {
+      border: none;
+    }
+
+    .ant-collapse:last-child {
+      border-bottom: 1px solid #d9d9d9;
+    }
+
+    .ant-collapse > .ant-collapse-item > .ant-collapse-header .ant-collapse-arrow {
+      top: 50%;
+      transform: translateY(-50%);
+      left: 10px;
+    }
+
+    .ant-collapse-content > .ant-collapse-content-box {
+      padding: 4px;
+    }
   }
 `;
