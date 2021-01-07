f8b7aacf9f8ebb4d4a66f852342397bfa25d4bb0
Robin Zimmer <robinzimmer1989@web.de>
2021-01-07 08:17:12 -0800

    render gtm conditionally

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -3,26 +3,28 @@ const React = require('react');
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents();
 
-  headComponents.push(
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GOOGLE_ANALYTICS}`}
-    ></script>
-  );
+  process.env.GATSBY_GOOGLE_ANALYTICS &&
+    headComponents.push(
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GOOGLE_ANALYTICS}`}
+      ></script>
+    );
 
-  headComponents.push(
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
+  process.env.GATSBY_GOOGLE_ANALYTICS &&
+    headComponents.push(
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', '${process.env.GATSBY_GOOGLE_ANALYTICS}');
       `,
-      }}
-    />
-  );
+        }}
+      />
+    );
 
   replaceHeadComponents(headComponents);
 };
