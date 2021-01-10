917357f4969c1b265770aed0a93b8b05e81c9abb
Robin Zimmer <robinzimmer1989@web.de>
2021-01-10 09:13:04 -0800

    fix console warning

--- jam-cms/src/store/configState.js
+++ jam-cms/src/store/configState.js
@@ -1,8 +1,6 @@
 import produce from 'immer';
 
 export const configReducer = (state, action) => {
-  const { payload } = action;
-
   return produce(state, (draft) => {
     switch (action.type) {
       default:
