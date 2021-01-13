347539e0346fe5631d0ce44f8e0fd1645604557a
Robin Zimmer <robinzimmer1989@web.de>
2021-01-13 07:59:33 -0800

    new styling

--- jam-cms/src/components/MenuBuilder.js
+++ jam-cms/src/components/MenuBuilder.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Button, Row, Col, Tree, Collapse, Space, Card, Tabs } from 'antd';
+import { Button, Tree, Collapse, Space, Card, Tabs, Divider } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
 // import app components
@@ -142,99 +142,83 @@ const MenuBuilder = (props) => {
 
   return (
     <Container>
-      <Row justify="space-between">
-        <Col span="11">
-          <Card
-            style={{ border: `1px solid ${colors.text.light}`, height: '360px', overflow: 'auto' }}
-          >
-            <Tabs defaultActiveKey="all" onChange={(v) => setFilter(v)}>
-              {Object.values(sites[siteID]?.postTypes).map((o) => {
-                return <Tabs.TabPane key={o.title} tab={o.title.toUpperCase()} />;
+      <Tabs defaultActiveKey="all" onChange={(v) => setFilter(v)}>
+        {Object.values(sites[siteID]?.postTypes).map((o) => {
+          return <Tabs.TabPane key={o.title} tab={o.title.toUpperCase()} />;
+        })}
+
+        <Tabs.TabPane key={'custom-link'} tab={'CUSTOM LINK'} />
+      </Tabs>
+
+      <Boxes>
+        <PostsBox>
+          {posts &&
+            Object.values(posts)
+              .filter(({ status }) => status !== 'trash')
+              .map(({ id, title, status, postTypeID }) => {
+                const badges = [];
+
+                if (status === 'draft') {
+                  badges.push(<Tag key="status" children={status} />);
+                }
+
+                return (
+                  <StyledListItem
+                    key={id}
+                    title={title}
+                    status={badges}
+                    hideImage
+                    actions={[
+                      <Button
+                        key="add"
+                        size="small"
+                        disabled={status === 'draft'}
+                        onClick={() =>
+                          setItems([
+                            ...items,
+                            {
+                              key: generateRandomString(),
+                              title,
+                              postTypeID,
+                              postID: id,
+                              children: [],
+                              url: generateSlug(postType, id, sites?.[siteID]?.frontPage, true),
+                            },
+                          ])
+                        }
+                        shape="circle"
+                      >
+                        <PlusOutlined />
+                      </Button>,
+                    ]}
+                  />
+                );
               })}
 
-              <Tabs.TabPane key={'custom-link'} tab={'CUSTOM LINK'} />
-            </Tabs>
-
-            <ItemsContainer>
-              <Space direction="vertical">
-                {posts &&
-                  Object.values(posts)
-                    .filter(({ status }) => status !== 'trash')
-                    .map(({ id, title, status, postTypeID }) => {
-                      const badges = [];
-
-                      if (status === 'draft') {
-                        badges.push(<Tag key="status" children={status} />);
-                      }
-
-                      return (
-                        <ListItem
-                          key={id}
-                          title={title}
-                          status={badges}
-                          hideImage
-                          actions={[
-                            <Button
-                              key="add"
-                              size="small"
-                              disabled={status === 'draft'}
-                              onClick={() =>
-                                setItems([
-                                  ...items,
-                                  {
-                                    key: generateRandomString(),
-                                    title,
-                                    postTypeID,
-                                    postID: id,
-                                    children: [],
-                                    url: generateSlug(
-                                      postType,
-                                      id,
-                                      sites?.[siteID]?.frontPage,
-                                      true
-                                    ),
-                                  },
-                                ])
-                              }
-                              shape="circle"
-                            >
-                              <PlusOutlined />
-                            </Button>,
-                          ]}
-                        />
-                      );
-                    })}
-
-                {filter === 'custom-link' && (
-                  <Space direction="vertical" size={20}>
-                    <Input
-                      label="Title"
-                      value={customLink.title}
-                      onChange={(e) => setCustomLink({ ...customLink, title: e.target.value })}
-                    />
-                    <Input
-                      label="Url"
-                      value={customLink.url}
-                      onChange={(e) => setCustomLink({ ...customLink, url: e.target.value })}
-                      placeholder="https://"
-                    />
-                    <Button
-                      style={{ marginBottom: 20 }}
-                      children={`Add`}
-                      type="primary"
-                      onClick={handleAddCustomLink}
-                    />
-                  </Space>
-                )}
-              </Space>
-            </ItemsContainer>
-          </Card>
-        </Col>
-
-        <Col span="12">
-          <Card
-            style={{ border: `1px solid ${colors.text.light}`, height: '360px', overflow: 'auto' }}
-          >
+          {filter === 'custom-link' && (
+            <Space direction="vertical" size={20}>
+              <Input
+                label="Title"
+                value={customLink.title}
+                onChange={(e) => setCustomLink({ ...customLink, title: e.target.value })}
+              />
+              <Input
+                label="Url"
+                value={customLink.url}
+                onChange={(e) => setCustomLink({ ...customLink, url: e.target.value })}
+                placeholder="https://"
+              />
+              <Button
+                style={{ marginBottom: 20 }}
+                children={`Add`}
+                type="primary"
+                onClick={handleAddCustomLink}
+              />
+            </Space>
+          )}
+        </PostsBox>
+        <MenuBox>
+          <div>
             <Tree
               className="draggable-tree"
               draggable
@@ -271,9 +255,10 @@ const MenuBuilder = (props) => {
                 );
               }}
             />
-          </Card>
-        </Col>
-      </Row>
+          </div>
+        </MenuBox>
+      </Boxes>
+
       <Button children={`Update`} onClick={handleSubmit} type="primary" />
     </Container>
   );
@@ -281,10 +266,6 @@ const MenuBuilder = (props) => {
 
 const Container = styled.div`
   overflow: auto;
-
-  .ant-col {
-    margin-bottom: 20px;
-  }
 
   .ant-tree {
     background-color: transparent;
@@ -304,6 +285,54 @@ const Container = styled.div`
   }
 `;
 
-const ItemsContainer = styled.div``;
+const Boxes = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: #fff;
+  margin-bottom: 20px;
+  border: 1px solid #d9d9d9;
+`;
+
+const PostsBox = styled.div`
+  background: #f7f7f7;
+  width: 320px;
+  height: 360px;
+  padding: 10px 10px 6px;
+  overflow: auto;
+`;
+
+const MenuBox = styled.div`
+  height: 360px;
+  flex: 1;
+  padding: 10px 10px 6px;
+  margin: 0 auto;
+  overflow: auto;
+
+  > div {
+    max-width: 400px;
+    margin: 0 auto;
+  }
+`;
+
+const StyledListItem = styled(ListItem)`
+  .ant-card {
+    box-shadow: none;
+    background: ${colors.background.light};
+    border: 1px solid #d9d9d9;
+    margin-bottom: 4px;
+  }
+
+  .ant-card-body {
+    padding: 0 16px;
+  }
+
+  .ant-list-item-action li {
+    padding: 0;
+  }
+
+  .ant-typography strong {
+    font-weight: 400;
+  }
+`;
 
 export default MenuBuilder;
