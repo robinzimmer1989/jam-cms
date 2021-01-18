c6d5c81aad83c74dc9ddcacc5c2173bb6e1cb418
Robin Zimmer <robinzimmer1989@web.de>
2021-01-18 12:21:53 -0800

    add margin to container if sidebar is open

--- jam-cms/src/components/PageWrapper.js
+++ jam-cms/src/components/PageWrapper.js
@@ -2,29 +2,32 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 // import app components
+
 import { useStore } from '../store';
 
-const PageWrapper = ({ template, onClick, children }) => {
+const PageWrapper = (props) => {
+  const { template, children } = props;
+
   const [
     {
-      editorState: { siteHasChanged, postHasChanged },
+      editorState: { siteHasChanged, postHasChanged, sidebar },
     },
   ] = useStore();
 
   return (
-    <Page onClick={onClick}>
+    <Container sidebar={!!sidebar}>
       {template ? (
         <Content disableLinks={siteHasChanged || postHasChanged}>{children}</Content>
       ) : (
         <div id="jam-cms">{children}</div>
       )}
-    </Page>
+    </Container>
   );
 };
 
-const Page = styled.div`
-  padding-top: 50px;
-  width: 100%;
+const Container = styled.div`
+  width: ${({ sidebar }) => (sidebar ? 'calc(100% - 320px)' : '100%')};
+  margin-left: ${({ sidebar }) => (sidebar ? '320px' : 0)};
   background: #fff;
 `;
 
