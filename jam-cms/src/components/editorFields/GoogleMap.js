576294db382615ad9067919d4783159d2ccec0a9
Robin Zimmer <robinzimmer1989@web.de>
2021-01-22 20:25:26 -0800

    google maps field

--- jam-cms/src/components/editorFields/GoogleMap.js
+++ jam-cms/src/components/editorFields/GoogleMap.js
@@ -1,43 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Geocode from 'react-geocode';
 import { Typography, Input, Space } from 'antd';
 import styled from 'styled-components';
+import { EnvironmentTwoTone as MarkerIcon } from '@ant-design/icons';
 
 import { colors } from '../../theme';
+
+const Marker = () => (
+  <Icon>
+    <MarkerIcon />
+  </Icon>
+);
 
 const Map = (props) => {
   const { site, value, onChange } = props;
 
   let defaultValue = value || { lat: 59.95, lng: 30.33, address: '' };
 
-  const [search, setSearch] = useState(defaultValue.address);
-
   useEffect(() => {
     site?.googleMapsApi && Geocode.setApiKey(site.googleMapsApi);
   }, []);
 
-  const handleSearch = async () => {
+  const handleSearch = async (address) => {
     try {
-      const result = await Geocode.fromAddress(search);
+      const result = await Geocode.fromAddress(address);
 
       const {
         geometry: {
           location: { lat, lng },
         },
         formatted_address,
-      } = result.results[0];
+      } = result?.results?.[0];
 
-      setSearch(formatted_address);
+      if ((lat, lng)) {
+        onChange({
+          address: formatted_address || '',
+          lat,
+          lng,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  const handleGeocode = async (lat, lng) => {
+    const result = await Geocode.fromLatLng(lat, lng);
+
+    const { formatted_address } = result?.results?.[0];
+
+    if (formatted_address) {
       onChange({
         address: formatted_address,
         lat,
         lng,
       });
-    } catch (err) {
-      console.log(err);
     }
+  };
+
+  const handleChange = (e) => {
+    onChange({ ...defaultValue, address: e.target.value });
   };
 
   return (
@@ -45,9 +68,9 @@ const Map = (props) => {
       {site?.googleMapsApi ? (
         <Space direction="vertical">
           <Input.Search
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            onSearch={handleSearch}
+            value={defaultValue.address}
+            onChange={handleChange}
+            onSearch={() => handleSearch(defaultValue.address)}
             placeholder={defaultValue?.address || 'Enter an address'}
           />
           <MapContainer>
@@ -60,7 +83,12 @@ const Map = (props) => {
               }}
               defaultZoom={11}
               center={[defaultValue?.lat, defaultValue?.lng]}
-            ></GoogleMapReact>
+              onClick={({ lat, lng }) => handleGeocode(lat, lng)}
+            >
+              {defaultValue?.lat && defaultValue?.lng && (
+                <Marker lat={defaultValue.lat} lng={defaultValue.lng} />
+              )}
+            </GoogleMapReact>
           </MapContainer>
         </Space>
       ) : (
@@ -74,6 +102,13 @@ const MapContainer = styled.div`
   height: 220px;
   width: 100%;
   background: ${colors.tertiary};
+`;
+
+const Icon = styled.div`
+  svg {
+    height: 26px;
+    width: 20px;
+  }
 `;
 
 export default Map;
