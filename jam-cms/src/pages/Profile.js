ea1991e5940fca79197e40da765e8fc9fb67dfea
Robin Zimmer <robinzimmer1989@web.de>
2021-01-26 21:19:50 -0800

    get data from store instead of local storage

--- jam-cms/src/pages/Profile.js
+++ jam-cms/src/pages/Profile.js
@@ -5,19 +5,20 @@ import { Card } from 'antd';
 import CmsLayout from '../components/CmsLayout';
 import Caption from '../components/Caption';
 
-import { getCurrentUser } from '../utils/auth';
 import { useStore } from '../store';
 
 const Home = () => {
-  const [{ config }] = useStore();
-
-  const user = getCurrentUser(config);
+  const [
+    {
+      authState: { authUser },
+    },
+  ] = useStore();
 
   return (
     <CmsLayout pageTitle={`Profile`}>
       <Card title={`Personal Information`}>
         <Caption children="Email" />
-        <p>{user.email}</p>
+        <p>{authUser?.email}</p>
       </Card>
     </CmsLayout>
   );
