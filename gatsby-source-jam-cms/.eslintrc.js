6c7e870741593fe93875645129eec86b921d7e59
Robin Zimmer <robinzimmer1989@web.de>
2021-02-22 21:21:37 -0800

    update eslint settings

--- gatsby-source-jam-cms/.eslintrc.js
+++ gatsby-source-jam-cms/.eslintrc.js
@@ -1,6 +1,26 @@
 module.exports = {
+  requireConfigFile: false,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb'],
   globals: {
     GATSBY_TEMPLATES_PATH: false,
     GATSBY_GLOBAL_OPTIONS_PATH: false,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'react/prop-types': 1,
   },
 };
