4040ae24ea8fc25e0223a9c2c1b0ecff3b411c57
Robin Zimmer <robinzimmer1989@web.de>
2021-04-06 18:07:00 -0700

    fix gatsby image

--- gatsby/src/templates/taxonomies/category/single.js
+++ gatsby/src/templates/taxonomies/category/single.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { graphql, Link } from 'gatsby';
+import { GatsbyImage } from 'jam-cms';
 
 // import app components
 import Layout from '../../../components/Layout';
 import Edges from '../../../components/Edges';
 import Banner from '../../../components/banner/Banner';
-
-import { colors } from '../../../theme';
 
 const Template = (props) => {
   const {
@@ -21,10 +20,61 @@ const Template = (props) => {
   return (
     <Layout {...props}>
       <Banner headline={name} height="small" />
-      <Edges size="sm"></Edges>
+      <Edges size="sm">
+        <Content>
+          <Posts>
+            {posts?.nodes &&
+              posts.nodes.map((o) => (
+                <Post key={o.id} to={o.uri}>
+                  <ImageContainer>
+                    {o?.featuredImage?.node && (
+                      <GatsbyImage objectFit="contain" image={o.featuredImage.node} />
+                    )}
+                  </ImageContainer>
+
+                  <h3>{o.title}</h3>
+                </Post>
+              ))}
+
+            <Post to={'/'} style={{ margin: 0, padding: 0, height: 0 }} />
+          </Posts>
+        </Content>
+      </Edges>
     </Layout>
   );
 };
+
+const Content = styled.div`
+  padding-top: 40px;
+  padding-bottom: 60px;
+`;
+
+const Posts = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Post = styled(Link)`
+  width: 100%;
+  margin-bottom: 40px;
+
+  @media (min-width: 640px) {
+    width: calc(100% / 2 - 10px);
+  }
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 120px;
+  background: #fff;
+  margin-bottom: 20px;
+
+  .gatsby-image-wrapper {
+    max-height: 100%;
+    max-width: 100%;
+  }
+`;
 
 export const Query = graphql`
   query Category($slug: String!) {
@@ -32,7 +82,19 @@ export const Query = graphql`
       name
       posts {
         nodes {
+          id
           title
+          uri
+          featuredImage {
+            node {
+              altText
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(width: 400, placeholder: BLURRED)
+                }
+              }
+            }
+          }
         }
       }
     }
