874374ecf92e2d65a5946df7b48062805aea3865
Robin Zimmer <robinzimmer1989@web.de>
2021-04-11 17:11:17 -0700

    update dashboard

--- jam-cms/src/pages/Dashboard.js
+++ jam-cms/src/pages/Dashboard.js
@@ -1,59 +1,84 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Alert, Space } from 'antd';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import { Alert, Space, List, Skeleton, Card, Typography } from 'antd';
 
 // import app components
 import CmsLayout from '../components/CmsLayout';
+import { actionMonitorServices } from '../services';
 import { useStore } from '../store';
-import { version } from '../../package.json';
 
 const Dashboard = () => {
   const [
     {
+      config,
       cmsState: { sites, siteID },
     },
   ] = useStore();
 
+  const [changes, setChanges] = useState(null);
+
+  const lastBuild = sites?.[siteID]?.deployment?.lastBuild;
+
   const errors = sites?.[siteID]?.errors;
 
+  useEffect(() => {
+    const loadChangesSinceLastBuild = async () => {
+      // Format date to timestamp (in miliseconds)
+      const timestamp = moment.utc(lastBuild, 'MM/DD/YYYY HH:mm:ss a').format('x');
+
+      const result = await actionMonitorServices.getChangesSinceLastBuild({ timestamp }, config);
+
+      if (result?.data?.actionMonitorActions?.nodes) {
+        // Remove internal entries from list
+        const fileredResults = result.data.actionMonitorActions.nodes.filter(
+          (o) => o.actionType !== 'DIFF_SCHEMAS'
+        );
+
+        setChanges(fileredResults);
+      }
+    };
+
+    loadChangesSinceLastBuild();
+  }, [lastBuild]);
+
   return (
-    <>
-      <CmsLayout pageTitle={`Dashboard`}>
-        <Content>
-          {errors && (
-            <Space direction="vertical">
-              {errors.map((o, i) => {
-                return (
-                  <Alert
-                    key={i}
-                    message={o.title}
-                    description={o.description}
-                    type="error"
-                    showIcon
-                  />
-                );
-              })}
-            </Space>
+    <CmsLayout pageTitle={`Dashboard`}>
+      <Space direction="vertical" size={40}>
+        {errors && (
+          <Space direction="vertical">
+            {errors.map((o, i) => {
+              return (
+                <Alert
+                  key={i}
+                  message={o.title}
+                  description={o.description}
+                  type="error"
+                  showIcon
+                />
+              );
+            })}
+          </Space>
+        )}
+
+        <Card title="Unpublished changes" style={{ maxHeight: 400, overflow: 'auto' }}>
+          {changes === null ? (
+            <Skeleton paragraph={{ rows: 4 }} active />
+          ) : changes?.length ? (
+            <List
+              dataSource={changes}
+              renderItem={(o) => (
+                <List.Item actions={[<span key="type">{o.actionType}</span>]}>
+                  <List.Item.Meta title={o.title} description={o.referencedNodeSingularName} />
+                </List.Item>
+              )}
+            />
+          ) : (
+            <Typography children={`No Changes`} />
           )}
-        </Content>
-        <Footer>{version}</Footer>
-      </CmsLayout>
-    </>
+        </Card>
+      </Space>
+    </CmsLayout>
   );
 };
-
-const Content = styled.div``;
-
-const Footer = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 50px;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 0 40px;
-`;
 
 export default Dashboard;
