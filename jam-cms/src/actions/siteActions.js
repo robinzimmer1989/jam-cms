6aae327591a454e3959748dde02070afb2ab9607
Robin Zimmer <robinzimmer1989@web.de>
2021-04-22 22:31:27 -0700

    fix update front page

--- jam-cms/src/actions/siteActions.js
+++ jam-cms/src/actions/siteActions.js
@@ -15,7 +15,7 @@ export const addSite = async ({ title, ownerID }, dispatch, config) => {
 };
 
 export const updateSite = async (
-  { id, globalOptions, deployment, title, siteUrl, googleMapsApi },
+  { id, globalOptions, frontPage, deployment, title, siteUrl, googleMapsApi },
   dispatch,
   config
 ) => {
@@ -23,6 +23,7 @@ export const updateSite = async (
     {
       id,
       globalOptions,
+      frontPage,
       deployment,
       title,
       siteUrl,
