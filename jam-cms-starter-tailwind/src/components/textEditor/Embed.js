020244290554def74c736d4f407b5bd868287823
Robin Zimmer <robinzimmer1989@web.de>
2021-04-30 21:52:58 -0700

    fix ssr error

--- jam-cms-starter-tailwind/src/components/textEditor/Embed.js
+++ jam-cms-starter-tailwind/src/components/textEditor/Embed.js
@@ -4,7 +4,9 @@ import ReactEmbed from 'react-embed';
 const Embed = (props) => {
   const { url } = props;
 
-  return <div className="py-8">{url && <ReactEmbed url={url} />}</div>;
+  return (
+    <div className="py-8">{url && typeof window !== 'undefined' && <ReactEmbed url={url} />}</div>
+  );
 };
 
 export default Embed;
