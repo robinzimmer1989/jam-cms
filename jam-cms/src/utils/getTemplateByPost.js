d18da2e469c568f97f05dcbc50d3ede5828dbf37
Robin Zimmer <robinzimmer1989@web.de>
2021-04-30 21:39:29 -0700

    fix potential identifier error

--- jam-cms/src/utils/getTemplateByPost.js
+++ jam-cms/src/utils/getTemplateByPost.js
@@ -1,7 +1,7 @@
 export default function getTemplateByPost(post, templates) {
   let template;
 
-  if (post?.template.includes('archive')) {
+  if (post?.template.includes('archive-')) {
     // The archive id is stored in the format 'archive-[post-type]'
     const postType = post.template.split('-')[1];
     template = templates?.postTypes?.[postType]?.archive;
