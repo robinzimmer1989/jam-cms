4a4c82c14ef90d2d931498583ff06a58c13c4a8c
Robin Zimmer <robinzimmer1989@web.de>
2021-05-09 11:00:36 -0700

    pass props to layout

--- jam-cms-starter-tailwind/src/pages/404.js
+++ jam-cms-starter-tailwind/src/pages/404.js
@@ -3,9 +3,9 @@ import React from 'react';
 // import app components
 import Layout from '../components/Layout';
 
-const NotFoundPage = () => {
+const NotFoundPage = (props) => {
   return (
-    <Layout>
+    <Layout {...props}>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="max-w-screen-sm sm:text-center sm:mx-auto">
           <h2 className="mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
