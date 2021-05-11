f947bd0735c5f92b71d61a65dbfc66dc0d6b9008
Robin Zimmer <robinzimmer1989@web.de>
2021-05-11 08:05:46 -0700

    simplify props

--- jam-cms/src/JamCMS.js
+++ jam-cms/src/JamCMS.js
@@ -13,12 +13,10 @@ import { StoreProvider } from './store';
 const Master = React.lazy(() => import('./components/Master'));
 
 const JamCMS = (props) => {
-  const { fields, source, siteID, settings, children: defaultComponent } = props;
-
   return (
     <React.Suspense fallback={<Loader />}>
-      <StoreProvider source={source} settings={settings} siteID={siteID}>
-        <Master fields={fields} defaultComponent={defaultComponent} />
+      <StoreProvider {...props}>
+        <Master defaultComponent={props.children} />
       </StoreProvider>
     </React.Suspense>
   );
