2a0a8a3daf98b252292da3c2c783d97ccb91b03f
Robin Zimmer <robinzimmer1989@web.de>
2021-05-19 16:57:04 -0700

    wrap in useMemo to prevent re-renders and flashing

--- jam-cms/src/components/GatsbyImage.js
+++ jam-cms/src/components/GatsbyImage.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { GatsbyImage as Image, getImage } from 'gatsby-plugin-image';
 
 const GatsbyImage = (props) => {
   const { image, alt = '', ...rest } = props;
 
-  let imageObject = image?.localFile && getImage(image.localFile);
+  // useMemo prevents the element from re-rendering when dealing with SVG images
+  // TODO: There might be a better solution for this in the future
+  const component = useMemo(() => {
+    let imageObject = image?.localFile && getImage(image.localFile);
 
-  if (!imageObject && image?.sourceUrl) {
-    imageObject = {
-      height: image?.height,
-      width: image?.width,
-      images: {
-        fallback: {
-          sizes: image?.sizes,
-          srcSet: image?.srcSet,
-          src: image?.sourceUrl,
-        },
-        sources: [
-          {
-            sizes: image?.sizes,
-            srcSet: image?.srcSet,
-            type: image?.mediaType,
+    if (!imageObject && image?.sourceUrl) {
+      imageObject = {
+        height: image?.height || 0,
+        width: image?.width || 0,
+        images: {
+          fallback: {
+            sizes: image?.sizes || '',
+            srcSet: image?.srcSet || '',
+            src: image?.sourceUrl,
           },
-        ],
-      },
-      layout: 'constrained',
-      placeholder: { fallback: '' },
-    };
-  }
+          sources: [
+            {
+              sizes: image?.sizes || '',
+              srcSet: image?.srcSet || '',
+              type: image?.mediaType || '',
+            },
+          ],
+        },
+        layout: 'constrained',
+        placeholder: { fallback: '' },
+      };
+    }
+    return imageObject ? <Image {...rest} image={imageObject} alt={alt} /> : null;
+  }, [props]);
 
-  return imageObject ? <Image {...rest} image={imageObject} alt={alt} /> : null;
+  return component;
 };
 
 export default GatsbyImage;
