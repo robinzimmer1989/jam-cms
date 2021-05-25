8679c147e6f82562c1cd9d8d751ce69adfbdcaa9
Robin Zimmer <robinzimmer1989@web.de>
2021-05-25 07:41:45 -0700

    move format site into utils function

--- jam-cms/src/services/siteServices.js
+++ jam-cms/src/services/siteServices.js
@@ -1,8 +1,6 @@
-import produce from 'immer';
-import { get } from 'lodash';
-
 // import app components
 import { db } from '.';
+import { formatSite } from '../utils';
 
 export const getSites = async ({}, dispatch, config) => {
   let result = await db('getSites', {}, dispatch, config);
@@ -21,7 +19,7 @@ export const getSite = async ({ siteID }, dispatch, config) => {
   let result = await db('getSite', { siteID }, dispatch, config);
 
   if (result) {
-    result = transformSite(result);
+    result = formatSite(result);
   }
 
   return result;
@@ -31,7 +29,7 @@ export const addSite = async ({ title }, dispatch, config) => {
   let result = await db('createSite', { title }, dispatch, config);
 
   if (result) {
-    result = transformSite(result);
+    result = formatSite(result);
   }
 
   return result;
@@ -59,7 +57,7 @@ export const updateSite = async (
   );
 
   if (result) {
-    result = transformSite(result);
+    result = formatSite(result);
   }
 
   return result;
@@ -69,7 +67,7 @@ export const deleteSite = async ({ id }, dispatch, config) => {
   let result = await db('deleteSite', { id }, dispatch, config);
 
   if (result) {
-    result = transformSite(result);
+    result = formatSite(result);
   }
 
   return result;
@@ -79,7 +77,7 @@ export const deploySite = async ({ id }, dispatch, config) => {
   let result = await db('deploySite', { id }, dispatch, config);
 
   if (result) {
-    result = transformSite(result);
+    result = formatSite(result);
   }
 
   return result;
@@ -89,7 +87,7 @@ export const syncFields = async ({ fields, apiKey }, dispatch, config) => {
   let result = await db('syncFields', { fields: JSON.stringify(fields), apiKey }, dispatch, config);
 
   if (result) {
-    result = transformSite(result);
+    result = formatSite(result);
   }
 
   return result;
@@ -98,39 +96,4 @@ export const syncFields = async ({ fields, apiKey }, dispatch, config) => {
 export const getUnpublishedChanges = async ({ siteID }, dispatch, config) => {
   const result = await db('getUnpublishedChanges', { siteID }, dispatch, config);
   return result;
-};
-
-const transformSite = (site) => {
-  const nextSite = produce(site, (draft) => {
-    // Convert posts and then post types to object structure
-    if (get(draft, `postTypes.items`)) {
-      draft.postTypes.items.map((o, i) => {
-        if (get(o, `posts.items`)) {
-          return (draft.postTypes.items[i].posts = draft.postTypes.items[i].posts.items.reduce(
-            (ac, a) => ({
-              ...ac,
-              [a.id]: a,
-            }),
-            {}
-          ));
-        }
-
-        return null;
-      });
-
-      draft.postTypes = draft.postTypes.items.reduce((ac, a) => ({ ...ac, [a.id]: a }), {});
-    }
-
-    // Convert taxonomies to object structure
-    if (get(draft, `taxonomies.items`)) {
-      draft.taxonomies = draft.taxonomies.items.reduce((ac, a) => ({ ...ac, [a.id]: a }), {});
-    }
-
-    // Convert forms to object structure
-    if (get(draft, `forms.items`)) {
-      draft.forms = draft.forms.items.reduce((ac, a) => ({ ...ac, [a.id]: a }), {});
-    }
-  });
-
-  return nextSite;
 };
