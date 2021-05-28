7faa08c163721a58bd72857bfebefb75721ec0aa
Robin Zimmer <robinzimmer1989@web.de>
2021-05-28 10:21:56 -0700

    fix missing date value

--- jam-cms/src/components/editorFields/DatePicker.js
+++ jam-cms/src/components/editorFields/DatePicker.js
@@ -5,9 +5,12 @@ import moment from 'moment';
 const DatePicker = (props) => {
   const { value, defaultValue, format = 'DD/MM/YYYY', onChange } = props;
 
+  // Add fallback date in case no value or default value is provided
+  const now = new Date();
+
   return (
     <AntDatePicker
-      value={moment(value || defaultValue, format)}
+      value={moment(value || defaultValue || now, format)}
       onChange={(value) => onChange(value ? value._d : '')}
       allowClear={false}
     />
