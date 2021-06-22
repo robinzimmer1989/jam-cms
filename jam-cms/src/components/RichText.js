bd68e06f951721259d2b726cff1b4d98b65f42f6
Robin Zimmer <robinzimmer1989@web.de>
2021-06-21 17:06:40 -0700

    fix: adding html elements in code editor

--- jam-cms/src/components/RichText.js
+++ jam-cms/src/components/RichText.js
@@ -9,27 +9,33 @@ const RichText = (props) => {
     return typeof string === 'string'
       ? Parser(string, {
           replace: (domNode) => {
-            if (domNode.type === 'tag' && domNode.name === 'a') {
-              if (domNode.attribs.href.includes('http')) {
-                return (
-                  <a href={domNode.attribs.href} target="_blank">
-                    {domNode.children.map((o) => parse(o.data))}
-                  </a>
-                );
-              } else if (
-                domNode.attribs.href.includes('tel:') ||
-                domNode.attribs.href.includes('mailto:')
-              ) {
-                return (
-                  <a href={domNode.attribs.href}>{domNode.children.map((o) => parse(o.data))}</a>
-                );
-              } else {
-                return (
-                  <Link to={domNode.attribs.href}>
-                    {domNode.children.map((o) => parse(o.data))}
-                  </Link>
-                );
+            console.log(domNode);
+            if (domNode.type === 'tag') {
+              if (domNode.name === 'a') {
+                if (domNode.attribs.href.includes('http')) {
+                  return (
+                    <a href={domNode.attribs.href} target="_blank">
+                      {domNode.children.map((o) => parse(o.data))}
+                    </a>
+                  );
+                } else if (
+                  domNode.attribs.href.includes('tel:') ||
+                  domNode.attribs.href.includes('mailto:')
+                ) {
+                  return (
+                    <a href={domNode.attribs.href}>{domNode.children.map((o) => parse(o.data))}</a>
+                  );
+                } else {
+                  return (
+                    <Link to={domNode.attribs.href}>
+                      {domNode.children.map((o) => parse(o.data))}
+                    </Link>
+                  );
+                }
               }
+
+              // Remove all special characters from string. This is necessary when using the code editor and start adding html elements.
+              domNode.name = domNode.name.replace(/[^a-zA-Z ]/g, '');
             }
           },
         })
