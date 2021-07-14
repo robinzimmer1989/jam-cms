96d8359de5051a3ffae9d4385d89bd09348aa838
Robin Zimmer <robinzimmer1989@web.de>
2021-07-14 08:42:01 -0700

    add check if post template exists

--- jam-cms/src/utils/getTemplateByPost.ts
+++ jam-cms/src/utils/getTemplateByPost.ts
@@ -1,7 +1,7 @@
 export default function getTemplateByPost(post: any, fields: any) {
   let template;
 
-  if (post?.template.includes('archive-')) {
+  if (post?.template?.includes('archive-')) {
     // The archive id is stored in the format 'archive-[post-type]'
     const postType = post.template.split('-')[1];
     template = fields?.postTypes?.[postType]?.templates?.archive;
