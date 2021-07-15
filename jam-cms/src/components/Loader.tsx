ae86a1f798a3c09a6d7a032fd3e73101ab6c3d3d
Robin Zimmer <robinzimmer1989@web.de>
2021-07-15 08:01:14 -0700

    add optional text

--- jam-cms/src/components/Loader.tsx
+++ jam-cms/src/components/Loader.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Spin } from 'antd';
+import { Spin, Typography } from 'antd';
 
-const Loader = () => {
+const Loader = (props: any) => {
+  const { text = '' } = props;
+
   return (
-    <LoadingContainer>
-      <Spin size="large" />
-    </LoadingContainer>
+    <Container>
+      <Content>
+        <Spin size="large" />
+        {text && <Typography.Text style={{ marginTop: 20 }}>{text}</Typography.Text>}
+      </Content>
+    </Container>
   );
 };
 
-const LoadingContainer = styled.div`
+const Container = styled.div`
   height: calc(100vh - 50px);
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 export default Loader;
