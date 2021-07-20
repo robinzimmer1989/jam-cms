3694bedd125171cd53e5ed2197f5b182467f36ca
Robin Zimmer <robinzimmer1989@web.de>
2021-07-20 11:00:20 -0700

    add basic starter for testing

---
+++ gatsby-starter-default/src/fields.js
@@ -0,0 +1,25 @@
+const fields = {
+  postTypes: [
+    {
+      id: "page",
+      title: "Page",
+      templates: [
+        {
+          id: "default",
+          label: "Default",
+          fields: [
+            {
+              id: "foo",
+              type: "wysiwyg",
+              label: "Bar",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  taxonomies: [],
+  themeOptions: [],
+}
+
+export default fields
