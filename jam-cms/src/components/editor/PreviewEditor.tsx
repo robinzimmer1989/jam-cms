a90daac668accd9452f0f47b6e8e7a98a379ff96
Robin Zimmer <robinzimmer1989@web.de>
2021-07-21 15:50:50 -0700

    fix previews

--- jam-cms/src/components/editor/PreviewEditor.tsx
+++ jam-cms/src/components/editor/PreviewEditor.tsx
@@ -1,42 +1,29 @@
-import React, { useEffect, useMemo } from 'react';
-import styled from 'styled-components';
-import { Button } from 'antd';
+import React, { useEffect, useState } from 'react';
 
 // import app components
 import EditorWrapper from './EditorWrapper';
 import Editor from './Editor';
 import Loader from '../Loader';
 
-import { auth, getPostID } from '../../utils';
+import { auth } from '../../utils';
 import { useStore } from '../../store';
 import { previewActions } from '../../actions';
 
 const PreviewEditor = (props: any) => {
-  const { defaultComponent } = props;
+  const [loading, setLoading] = useState(true);
 
   const [
     {
       config,
-      cmsState: { sites, siteID },
+      cmsState: { siteID },
       editorState: { post },
     },
     dispatch,
   ] = useStore();
 
-  const postID = useMemo(() => {
-    return getPostID(sites[siteID]);
-  }, [window.location.pathname]);
-
-  const previewID = auth.getPreviewID();
-
-  // We need to check if the parentID is equal to the postID
-  const loaded = postID && post?.id === postID;
-
   useEffect(() => {
     const loadPost = async () => {
-      if (!postID) {
-        return;
-      }
+      const previewID = auth.getPreviewID();
 
       const result = await previewActions.getPostPreview({ siteID, previewID }, dispatch, config);
 
@@ -44,35 +31,22 @@ const PreviewEditor = (props: any) => {
         dispatch({ type: 'ADD_POST', payload: { ...result, siteID } });
         dispatch({ type: 'ADD_EDITOR_POST', payload: { ...result, siteID } });
       }
+
+      setLoading(false);
     };
 
     loadPost();
 
     return () => dispatch({ type: `CLEAR_EDITOR` });
-  }, [postID]);
-
-  if (!postID) {
-    return defaultComponent;
-  }
+  }, []);
 
   return (
     <>
-      <EditorWrapper loaded={loaded}>
-        {loaded ? <Editor postID={postID} {...props} /> : <Loader text="Load Post" />}
+      <EditorWrapper loaded={!loading}>
+        {loading ? <Loader text="Load Preview" /> : <Editor postID={post?.id} {...props} />}
       </EditorWrapper>
-
-      <PreviewBanner children={`Preview`} type="primary" />
     </>
   );
 };
-
-const PreviewBanner = styled(Button)`
-  position: fixed;
-  left: 0;
-  top: 50%;
-  z-index: 9999;
-  transform: rotate(270deg) translate(-50%, 50%);
-  transform-origin: left;
-`;
 
 export default PreviewEditor;
--- jam-cms/src/components/router/PreviewRouter.tsx
+++ jam-cms/src/components/router/PreviewRouter.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import styled from 'styled-components';
+import { Button } from 'antd';
 
 // import components
 import PreviewEditor from '../editor/PreviewEditor';
@@ -14,16 +16,28 @@ const PreviewRouter = (props: any) => {
     },
   ] = useStore();
 
-  // Wait until site is loaded
-  if (!sites[siteID]) {
-    return <Loader text="Load Website" />;
-  }
-
   return (
-    <Router>
-      <PreviewEditor path={'*'} {...props} />
-    </Router>
+    <>
+      {sites[siteID] ? (
+        <Router>
+          <PreviewEditor path={'*'} {...props} />
+        </Router>
+      ) : (
+        <Loader text="Load Preview" />
+      )}
+
+      <PreviewBanner children="Preview" type="primary" />
+    </>
   );
 };
+
+const PreviewBanner = styled(Button)`
+  position: fixed;
+  left: 0;
+  top: 50%;
+  z-index: 9999;
+  transform: rotate(270deg) translate(-50%, 50%);
+  transform-origin: left;
+`;
 
 export default PreviewRouter;
