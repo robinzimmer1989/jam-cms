cbbe16f4771d5a320c3611f6482baa5454ca6857
Robin Zimmer <robinzimmer1989@web.de>
2021-09-09 09:29:17 -0700

    fix: remove unused import

--- jam-cms/src/components/Loader.tsx
+++ jam-cms/src/components/Loader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Spin, Typography } from 'antd';
+import { Spin } from 'antd';
 
 const Loader = (props: any) => {
   const { text = '', height = '100vh', py = 0 } = props;
